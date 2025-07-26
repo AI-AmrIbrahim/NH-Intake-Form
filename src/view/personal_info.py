634b36b154eac533e57b103a36607f2f884420e7
Amr Ibrahim (work) <amrkhalid111999@gmail.com>
2025-07-26 15:12:43 -0400

    Clear function works for personal_info

--- src/view/personal_info.py
+++ src/view/personal_info.py
@@ -7,22 +7,22 @@ def personal_info_form(user_profile, user_status):
         st.header("👤 Personal Information")
 
         if user_status == "No, I have not filled out the intake form before":
-            email_input = st.text_input("Email")
+            email_input = st.text_input("Email", key="email")
             st.write("Name")
             col1, col2 = st.columns(2)
             with col1:
-                first_name_input = st.text_input("First Name", value=user_profile.get("first_name", ""))
+                first_name_input = st.text_input("First Name", key="first_name")
             with col2:
-                last_name_input = st.text_input("Last Name", value=user_profile.get("last_name", ""))
+                last_name_input = st.text_input("Last Name", key="last_name")
         else: # Returning User
             email_input = user_profile.get("email", "") # Use loaded profile data
             st.text_input("Email", value=email_input, disabled=True)
             st.write("Name")
             col1, col2 = st.columns(2)
             with col1:
-                first_name_input = st.text_input("First Name", value=user_profile.get("first_name", ""), disabled=True)
+                first_name_input = st.text_input("First Name", key="first_name", disabled=True)
             with col2:
-                last_name_input = st.text_input("Last Name", value=user_profile.get("last_name", ""), disabled=True)
+                last_name_input = st.text_input("Last Name", key="last_name", disabled=True)
 
         st.write("Date of Birth")
         today = datetime.date.today()
@@ -34,20 +34,28 @@ def personal_info_form(user_profile, user_status):
             try:
                 user_dob = datetime.datetime.strptime(user_dob, "%Y-%m-%d").date()
             except ValueError:
-                user_dob = None # Handle invalid format
-        
+                user_dob = None
+
         initial_year = user_dob.year if user_dob else 1990
         initial_month_index = user_dob.month - 1 if user_dob else 0
         initial_day_index = user_dob.day - 1 if user_dob else 0
 
+        # Avoid widget key conflict
+        if "dob_year" not in st.session_state:
+            st.session_state.dob_year = initial_year
+        if "dob_month" not in st.session_state:
+            st.session_state.dob_month = month_names[initial_month_index]
+        if "dob_day" not in st.session_state:
+            st.session_state.dob_day = initial_day_index + 1
+
         col1, col2, col3 = st.columns(3)
         with col1:
-            month_name = st.selectbox("Month", month_names, index=initial_month_index)
+            month_name = st.selectbox("Month", month_names, key="dob_month")
             month = month_names.index(month_name) + 1
         with col2:
-            day = st.selectbox("Day", list(range(1, 32)), index=initial_day_index)
+            day = st.selectbox("Day", list(range(1, 32)), key="dob_day")
         with col3:
-            year = st.selectbox("Year", list(range(1920, today.year + 1)), index=list(range(1920, today.year + 1)).index(initial_year))
+            year = st.selectbox("Year", list(range(1920, today.year + 1)), key="dob_year")
 
         try:
             dob = datetime.date(year, month, day)
@@ -55,17 +63,25 @@ def personal_info_form(user_profile, user_status):
             st.error("Please enter a valid date of birth.")
             dob = None
 
+
+        # Optional: set defaults in session state if not set already
+        if "height_ft" not in st.session_state:
+            st.session_state["height_ft"] = 5  # default to 5 feet
+        if "height_in" not in st.session_state:
+            st.session_state["height_in"] = 6  # default to 6 inches
+
         st.write("Height")
         col1, col2 = st.columns(2)
         with col1:
-            height_ft = st.selectbox("Feet", list(range(4, 7)), 1)
+            height_ft = st.selectbox("Feet", list(range(4, 7)), key="height_ft")
         with col2:
-            height_in = st.selectbox("Inches", list(range(0, 12)), 6)
+            height_in = st.selectbox("Inches", list(range(0, 12)), key="height_in")
 
-        weight_input = st.text_input("Weight (in lbs)", value=str(user_profile.get("weight_lbs", "")))
+
+        weight_input = st.text_input("Weight (in lbs)", key="weight_lbs")
         st.write("") # Adds a little vertical space
         sex_options = ('Male', 'Female')
-        sex = st.radio("Biological Sex", sex_options, index=sex_options.index(user_profile.get("sex", "Male")))
+        sex = st.radio("Biological Sex", sex_options, index=sex_options.index(user_profile.get("sex", "Male")), key="sex")
 
         return {
             "email": email_input,
